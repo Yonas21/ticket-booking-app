6b1495cb8ee724a3425f289e4f0d237b69995189
Yonas Alem <yonalem21@gmail.com>
2025-08-19 13:27:36 +0300

    feat: Implement bus tracking and enhance date formatting
    Introduce a new bus tracking page and integrate it into the application flow.
    
    This commit also centralizes date and time formatting into a new `formatDateTime` utility, applying it consistently across `BusCard`, `BookingConfirmationPage`, `BookingPage`, and `SearchResultsPage` for improved consistency and maintainability.
    
    Additionally, refactor the `SeatSelection` component to use custom CSS for styling instead of Bootstrap classes, providing more control over its appearance.

--- src/data/date.ts
+++ src/data/date.ts
@@ -4,10 +4,9 @@ export const formatDateTime = (dateTimeString: string, type:string) => {
     if (isNaN(date.getTime())) return dateTimeString; // Return original if invalid date
 
     if (type === 'date') {
-      return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+      return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
     } else if (type === 'time') {
       return date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: true });
     }
     return dateTimeString;
-  };
-  
+};
