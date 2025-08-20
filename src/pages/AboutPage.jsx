7824a08d495eefc087f830520bb6af59b156408b
Yonas21 <yonalem21@gmail.com>
2025-08-20 22:07:09 +0300

    feat(ui): add dark mode styles to AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,20 +6,20 @@ const AboutPage = () => {
 
   return (
     <div className="container slick-design py-5">
-      <h2 className="mb-4">{t('common.aboutUs')}</h2>
-      <p>
+      <h2 className="mb-4 mt-16 text-gray-900 dark:text-white">{t('common.aboutUs')}</h2>
+      <p className="text-gray-700 dark:text-gray-300">
         {t('common.aboutUsContent1')}
       </p>
-      <p>
+      <p className="text-gray-700 dark:text-gray-300">
         {t('common.aboutUsContent2')}
       </p>
-      <p>
+      <p className="text-gray-700 dark:text-gray-300">
         {t('common.aboutUsContent3')}
       </p>
-      <p>
+      <p className="text-gray-700 dark:text-gray-300">
         {t('common.sincerely')},
       </p>
-      <p>
+      <p className="text-gray-700 dark:text-gray-300">
         {t('common.theBusTicketTeam')}
       </p>
     </div>
